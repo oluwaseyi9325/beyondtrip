418cf5002ba6b9d70435b045200bfc514d1a50b0
oluwaseyi9325 <adedokunseyi96@gmail.com>
2025-12-03 12:11:58 +0100

    feat: change icon

--- public/assets/icons/profile.tsx
+++ public/assets/icons/profile.tsx
@@ -9,7 +9,7 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
     {...props}
   >
     <path
-      fill="#27458F"
+      fill="currentColor"
       fillRule="evenodd"
       d="M10 0a10 10 0 0 1 10 10c0 5.523-4.477 10-10 10S0 15.523 0 10 4.477 0 10 0Zm1 11H9a6.001 6.001 0 0 0-5.518 3.64A7.99 7.99 0 0 0 10 18a7.99 7.99 0 0 0 6.518-3.36A6.002 6.002 0 0 0 11 11Zm-1-8a3 3 0 1 0 0 6 3 3 0 0 0 0-6Z"
       clipRule="evenodd"
